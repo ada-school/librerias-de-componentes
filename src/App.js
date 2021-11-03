1e4b3fe4698f1fb8894ad36864c4d59991d5d705
Valentina Bacherer <valentina.bacherer@gmail.com>
2021-11-03 09:46:28 -0300

    create login card

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
+import { Heading } from '@chakra-ui/react';
+import { Input } from '@chakra-ui/react';
+import {
+  Button,
+  ButtonGroup,
+  HStack,
+  useColorMode,
+  useColorModeValue,
+} from '@chakra-ui/react';
 
 function App() {
+  const { toggleColorMode } = useColorMode();
+  const backgroundColor = useColorModeValue('gray.200', 'gray.700');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex height='100vh' justifyContent='center' alignItems='center'>
+      <Box maxW='xs' bg={backgroundColor} p={6} borderRadius='md'>
+        <Heading size='lg' mb={3}>
+          Log In
+        </Heading>
+        <Input
+          placeholder='User'
+          variant='filled'
+          mb={3}
+          type='text'
+          autoComplete='off'
+        />
+        <Input
+          placeholder='Password'
+          variant='filled'
+          mb={6}
+          type='password'
+          autoComplete='off'
+        />
+        <HStack spacing='10' variant='outline' justify='center'>
+          <Button colorScheme='blue'>Login</Button>
+          <Button colorScheme='blue' onClick={toggleColorMode}>
+            Dark Mode
+          </Button>
+        </HStack>
+      </Box>
+    </Flex>
   );
 }
 
